ea1184b0d5aff09457a53a3d3cc5e129c2888ed6
claudiu87 <claudiu_iii@yahoo.com>
2022-02-13 18:10:10 +0200

    ad MQTT server

--- app.js
+++ app.js
@@ -1,197 +1,274 @@
-
+//==========================================================================
 // The following modules are required by nodejs
+var mqtt = require('mqtt');
 var http = require('http');
 var https = require('https');
-const serialport = require("serialport");                               
+const serialport = require("serialport");
 const Readline = require('@serialport/parser-readline')
 const bodyParser = require('body-parser');
-const express =require("express");
-var app =express();
+const express = require("express");
+var app = express();
 const fs = require('fs');
-var logData, i=0;
+var logData, i = 0;
+
+
+//==========================================================================
 
 
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));                        
-app.use(bodyParser.json());
-
-// Serial port is configured
-var port= new serialport('/dev/ttyACM0',{                               
-baudRate: 57600,
-});
-const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
-
-
-// Get the current date
-
-function getDate(){
-
-let ts = Date.now();
-let date_ob = new Date(ts);
-let date = date_ob.getDate();
-let month = date_ob.getMonth() + 1;
-let year = date_ob.getFullYear();
-let hour =date_ob.getHours();
-let minutes = date_ob.getMinutes();
-let seconds = date_ob.getSeconds();
-return [year,month,date,hour,minutes,seconds];
+//==========================================================================
+// MQTT clinet options
+var optionsMosquitto = {
+  host: 'mygardenauto.duckdns.org',
+  port: 8883,
+  protocol: 'mqtts',
+  rejectUnauthorized: false
 }
 
+//initialize the MQTT client
+var client = mqtt.connect(optionsMosquitto);
+
+//setup the MQTT callbacks
+client.on('connect', function() {
+  console.log('Connected to MQTT server');
+});
+
+client.on('error', function(error) {
+  console.log(error);
+});
+
+client.on('message', function(topic, message) {
+  //Called each time a message is received
+  console.log('Received message:', topic, message.toString());
+});
+
+// subscribe to topic room/temperature and room/humidity);
+client.subscribe('room/temperature');
+client.subscribe('room/humidity');
+
+// publish message 'Hi here is nodejs' to topic 'room/temperature'
+//client.publish('room/temperature', 'Hi here is nodejs');
+
+//==========================================================================
+
+
+
+//==========================================================================
+// app use
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+//==========================================================================
+
+
+
+// Serial port is configured
+//==========================================================================
+var port = new serialport('/dev/ttyACM0', {
+  baudRate: 57600,
+});
+const parser = port.pipe(new Readline({
+  delimiter: '\r\n'
+}));
+//==========================================================================
+
+
+
+//==========================================================================
+// Get the current date function
+function getDate() {
+
+  let ts = Date.now();
+  let date_ob = new Date(ts);
+  let date = date_ob.getDate();
+  let month = date_ob.getMonth() + 1;
+  let year = date_ob.getFullYear();
+  let hour = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+  return [year, month, date, hour, minutes, seconds];
+}
+//==========================================================================
+
+
+//==========================================================================
 // Disk data from the local file jsonData.json is used to configure the arduino at application startup
-	fs.readFile('./jsonData.json', 'utf8', (err, data) => {
-		if (err) {
-        console.log(`Error reading file from disk: ${err}`);
-        } else {
-			
-        console.log("Disk data: "+data);
-        port.write(data);
-        }
-        });
-    
-    	fs.readFile('./datalog.json', 'utf8', (err, data) => {
-		if (err) {
-        console.log(`Error reading file from disk: ${err}`);
-        } else {
-						
-        logData=data;
-        console.log("Disk logData: "+logData);
-        logData= JSON.parse(logData);
-        
-    
-        }
-        });
+fs.readFile('./jsonData.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log(`Error reading file from disk: ${err}`);
+  } else {
+
+    console.log("Disk data: " + data);
+    port.write(data);
+  }
+});
+
+fs.readFile('./datalog.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log(`Error reading file from disk: ${err}`);
+  } else {
+
+    logData = data;
+    //console.log("Disk logData: " + logData);
+    logData = JSON.parse(logData);
 
 
+  }
+});
+//==========================================================================
+
+
+//==========================================================================
 //logs the measured data
-function saveData(data){
-	
-		  
-  console.log("I am doing my 5 minutes log");
-  var [year,month,date,hour,minutes,seconds]=getDate();
-  logDataParse=logData;
-  logDataParse.time.push(year + "-" + month + "-" + date + " " + hour + ":" + minutes);	  	  
-  data=JSON.parse(data);
+function saveData(data) {
+
+  //console.log("I am doing my 5 minutes log");
+  var [year, month, date, hour, minutes, seconds] = getDate();
+  logDataParse = logData;
+  logDataParse.time.push(year + "-" + month + "-" + date + " " + hour + ":" + minutes);
+  data = JSON.parse(data);
   logDataParse.vBat.push(data.AN0);
-  logDataParse.temp.push(data.AN1);
-  console.log(JSON.stringify(logData));
-  i=i+1;
-  console.log(i);
-  
-  if(i==96){
-	  fs.writeFile('datalog.json',JSON.stringify(logData), function(err){
+
+
+  //temperature is calculated based on the AN1 value (Steinhart–Hart equation)
+  let A = 0.000871812,
+    B = 0.000257595,
+    C = 0.000000142252;
+  let R1 = 4300,
+    R2 = 0,
+    temp = 0,
+    v = 0,
+    logR2 = 0;
+  v = 5 / 1024 * data.AN1;
+  R2 = R1 / (5 / v - 1);
+  logR2 = Math.log(R2);
+  temp = 1 / (A + B * logR2 + C * Math.pow(logR2, 3));
+  temp = temp - 273.15;
+  temp = temp.toFixed(2);
+  logDataParse.temp.push(temp);
+
+
+  //console.log(JSON.stringify(logData));
+  i = i + 1;
+  //console.log(i);
+
+  if (i == 96) {
+    fs.writeFile('datalog.json', JSON.stringify(logData), function(err) {
       if (err) {
-      throw err;
-      }});
+        throw err;
+      }
+    });
 
-	  i=0;
-	  
-	  }	
-	}
+    i = 0;
 
+  }
+}
+//==========================================================================
+
+
+//==========================================================================
 // Runs a function at an interval of 5 min
 
-var minutes = 5, the_interval = minutes * 60 * 1000;
+var minutes = 5,
+  the_interval = minutes * 60 * 1000;
 setInterval(function() {
-  
+
   port.write("statusCheck");
-  parser.once("data", saveData); 
+  parser.once("data", saveData);
 
-}, the_interval);  
+}, the_interval);
+//==========================================================================
 
 
-    
+
+// GET requests
+//==========================================================================
 // The index.html is sent to the clinet
-app.get("/", function(req, res){
-	res.sendFile(__dirname+"/index.html");
-	});
-
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
 
 // Get test arduino
-app.get("/arduino", function(req, res){
-	res.send("Hello, this is the server");
-	});
+app.get("/arduino", function(req, res) {
+  res.send("Hello, this is the server");
+});
 
 
+// This get function configure the html "view" when the page is reloaded, it configurea the html/css based on the data that are read from arduino.
+app.get("/initAtReset", function(req, res) {
 
-// This get function configure the html "view" when the page is reloaded, it configurea the html/css based on the data that are read from arduino. 	
-app.get("/initAtReset", function(req, res){
-	
-	port.write("statusCheck");
-	parser.once("data", function(data){
-	res.json(data);
-	console.log("Data sent to clinet: "+ data)		
-	})  
-
-	});	
-	
-	
-app.get("/chart", function(req, res){
-	
-   // logData= JSON.parse(logData);
-    
-		  
-/*	var dataPoints =[];
-	for (var i=0; i<logData.time.length; i++){
-	    dataPoints.push({
-		   x:logData.time[i],
-		   y:logData.vBat[i]
-		});
-	};
-	
-	dataPoints =JSON.stringify(dataPoints);
-	//console.log(dataPoints);	
-	res.json(dataPoints); */
-	
-	var timeDataPoints =[];
-	var vBatDataPoints =[];
-	var tempDataPoints =[];
-	
-    for (var i=0; i<logData.time.length; i++){
-	     timeDataPoints.push(logData.time[i]);
-	     vBatDataPoints.push(logData.vBat[i]);
-	     tempDataPoints.push(logData.temp[i]/10);
-		};
-	
-		
-    var data= {time: timeDataPoints,
-  	           vBat: vBatDataPoints,
-  	           temp: tempDataPoints
-  	           };
-  	           
-  	//console.log(data);
-	res.json(JSON.stringify(data));
-		
-	});
-	
-	
-// This post function received the data from index.js send them to arduino reads them back and save them to a local file jsonData.json.
-app.post("/app", function(req,res){
-	
-	port.write(JSON.stringify(req.body));	
-    parser.once('data', function(data){
-    console.log("Data sent to arduino: "+data);
+  port.write("statusCheck");
+  parser.once("data", function(data) {
     res.json(data);
-    
-    fs.writeFile('jsonData.json',JSON.stringify(req.body), function(err){
-    if (err) {
+    console.log("Data sent to clinet: " + data)
+  })
+
+});
+
+app.get("/chart", function(req, res) {
+
+
+  var timeDataPoints = [];
+  var vBatDataPoints = [];
+  var tempDataPoints = [];
+
+  for (var i = 0; i < logData.time.length; i++) {
+    timeDataPoints.push(logData.time[i]);
+    vBatDataPoints.push(logData.vBat[i]);
+    tempDataPoints.push(logData.temp[i]);
+  };
+
+
+  var data = {
+    time: timeDataPoints,
+    vBat: vBatDataPoints,
+    temp: tempDataPoints
+  };
+
+
+  res.json(JSON.stringify(data));
+
+});
+
+//==========================================================================
+
+
+// POST requests
+//==========================================================================
+// This post function received the data from index.js send them to arduino, reads them back and save them to a local file jsonData.json.
+app.post("/app", function(req, res) {
+
+  port.write(JSON.stringify(req.body));
+  parser.once('data', function(data) {
+    console.log("Data sent to arduino: " + data);
+    res.json(data);
+
+    fs.writeFile('jsonData.json', JSON.stringify(req.body), function(err) {
+      if (err) {
         throw err;
-        }
-    }); 
+      }
     });
- });	   
- 
- 
+  });
+});
+//==========================================================================
 
- 
 
-var privateKey = fs.readFileSync(__dirname + '/certs/key.pem', 'utf8');
-var certificate = fs.readFileSync(__dirname + '/certs/cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+
+// The paths to certs are define and the http and https server is started
+//==========================================================================
+var privateKey = fs.readFileSync(__dirname + '/certs/server.key', 'utf8');
+var certificate = fs.readFileSync(__dirname + '/certs/server.crt', 'utf8');
+var credentials = {
+  key: privateKey,
+  cert: certificate
+};
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(3080);
 httpsServer.listen(3443);
+//==========================================================================
--- public/JS/index.js
+++ public/JS/index.js
@@ -1,290 +1,306 @@
 var resetChartZoom;
 
 
-// Chart area ------------------------------------------------------------------------------------
+// Chart is configured
+//==========================================================================
 
-function chart(dataPoints){
+function chart(dataPoints) {
 
-console.log(dataPoints.time);
-console.log(dataPoints.vBat);
-console.log(dataPoints.temp);
+  console.log(dataPoints.time);
+  console.log(dataPoints.vBat);
+  console.log(dataPoints.temp);
 
-	
-	
-const zoomOptions = {
-  limits: {
-    x: {min: 0, max: dataPoints.temp.length, minRange: 10},
-    y: {min: 0, max: 500, minRange: 10}
-  },
-  pan: {
-    enabled: true,
-    onPanStart({chart, point}) {
- 
+
+  const zoomOptions = {
+    limits: {
+      x: {
+        min: 0,
+        max: dataPoints.temp.length,
+        minRange: 10
+      },
+      y: {
+        min: 0,
+        max: 1500,
+        minRange: 1
+      }
     },
-    mode: 'xy',
-  },
-  zoom: {
-    wheel: {
+    pan: {
       enabled: true,
-    },
-    pinch: {
-      enabled: true
-    },
-  }
-};	
-	
-	
-const ctx = $('#myChart');
+      onPanStart({
+        chart,
+        point
+      }) {
 
-const myChart = new Chart(ctx, {
-	
+      },
+      mode: 'xy',
+    },
+    zoom: {
+      wheel: {
+        enabled: true,
+      },
+      pinch: {
+        enabled: true
+      },
+    }
+  };
+
+
+  const ctx = $('#myChart');
+
+  const myChart = new Chart(ctx, {
+
     type: 'line',
     data: {
-		//labels: ["2021-12-6 0:30","2021-12-6 0:35","2021-12-6 0:40","2021-12-6 0:45","2021-12-6 0:50","2021-12-6 0:55","2021-12-6 0:60"],
-		labels: dataPoints.time,
-        datasets:[
-        
-        {	
-        label: 'Battery voltage [V]',
-        //data: [65, 59, 80, 81, 56, 55, 40],
-        data: dataPoints.vBat,
-        fill: false,
-        borderColor: 'black',
-        backgroundColor: 'black',
-        tension: 0.1,
-        pointRadius:1,
-        yAxisID: 'y',
-        //color: 'red',
-        },
-        
-        
-        {	
-        label: 'Temperature [°C]',
-        //data: [65, 59, 80, 81, 56, 55, 40],
-        data: dataPoints.temp,
-        fill: false,
-        borderColor: '#5aa897',
-        backgroundColor: '#5aa897',
-        tension: 0.1,
-        pointRadius:1,
-        yAxisID: 'y1',
-        }
-        ]
-        
-    },
-  
-    options: {
-		
-	animation: false,
-		
-	scales: {
-           y: {
-               beginAtZero: true,
-               position:'left',
-               ticks: {
-                       color: "black",
-                       font:{
-						   // weight: 'bold',
-						    size: 14
-						   }
-                      }
-              },
-           y1:{
-               beginAtZero: true,
-               position:'right',
-               ticks: {
-                       color: "#5aa897",
-                       font:{
-						   // weight: 'bold',
-						    size: 14
-						   }
-                      }
-              },  
-              
-              
-           },	
-	
-    plugins: {
-      zoom: zoomOptions,
-      legend: {
-                labels: {
-              
-                    font: {
-                        size: 20
-                    }
-                }
-            }
-    }
-    
-    
-  }
- 
-});
+      //labels: ["2021-12-6 0:30","2021-12-6 0:35","2021-12-6 0:40","2021-12-6 0:45","2021-12-6 0:50","2021-12-6 0:55","2021-12-6 0:60"],
+      labels: dataPoints.time,
+      datasets: [
 
-function resetZoom(){
-	   myChart.resetZoom(); 
-	}
-resetChartZoom= resetZoom;
+        {
+          label: 'Battery voltage [V]',
+          //data: [65, 59, 80, 81, 56, 55, 40],
+          // data: dataPoints.vBat,
+          fill: false,
+          borderColor: 'black',
+          backgroundColor: 'black',
+          tension: 0.1,
+          pointRadius: 3,
+          yAxisID: 'y',
+          //color: 'red',
+        },
+
+
+        {
+          label: 'Temperature [°C]',
+          //data: [65, 59, 80, 81, 56, 55, 40],
+          data: dataPoints.temp,
+          fill: false,
+          borderColor: '#5aa897',
+          backgroundColor: '#5aa897',
+          tension: 0.1,
+          pointRadius: 3,
+          yAxisID: 'y1',
+        }
+      ]
+
+    },
+
+    options: {
+
+      animation: false,
+
+      scales: {
+        y: {
+          beginAtZero: true,
+          position: 'left',
+          ticks: {
+            color: "black",
+            font: {
+              // weight: 'bold',
+              size: 14
+            }
+          }
+        },
+        y1: {
+          beginAtZero: true,
+          position: 'right',
+          ticks: {
+            color: "#5aa897",
+            font: {
+              // weight: 'bold',
+              size: 14
+            }
+          }
+        },
+
+
+      },
+
+      plugins: {
+        zoom: zoomOptions,
+        legend: {
+          labels: {
+
+            font: {
+              size: 20
+            }
+          }
+        }
+      }
+
+
+    }
+
+  });
+
+
+  resetChartZoom = function resetZoom() {
+    myChart.resetZoom();
+  }
 
 }
 
 
 
-function chartPointSize(){
-console.log("This is the point size function");
-console.log($(".numberOfPoints").val());
-	}
+function chartPointSize() {
+  console.log("This is the point size function");
+  console.log($(".numberOfPoints").val());
+}
 
 
 
 
 // this function is called from index.html
 
-function resetZ(){
-    resetChartZoom();	
-	}
-	
-	
-// Chart area ------------------------------------------------------------------------------------
+function resetZ() {
+  resetChartZoom();
+}
+
+//==========================================================================
 
 
 
+//GET requests
+//==========================================================================
 // GET chart data when the page is loaded.
-  $.ajax({                       
-          traditional: true,
-          url: '/chart',
-          type: 'GET',
-         // contentType: 'application/json',
-         // dataType: 'json',
-          success: function(dataPoints){
-		  dataPoints=JSON.parse(dataPoints);
-		  console.log(dataPoints);
-		  chart(dataPoints);
-		  }	  	  
-  });
-  
-  
-// GET request when the page is load to initialize the value of the buttons
-  $.ajax({                       
-          traditional: true,
-          url: '/initAtReset',
-          type: 'GET',
-         // contentType: 'application/json',
-         // dataType: 'json',
-          success: function(getData){
-		  dataFromServer=JSON.parse(getData);	   
-		  console.log(getData);
-		  evaluateState(getData);
-		  }
-  } );
+$.ajax({
+  traditional: true,
+  url: '/chart',
+  type: 'GET',
+  // contentType: 'application/json',
+  // dataType: 'json',
+  success: function(dataPoints) {
+    dataPoints = JSON.parse(dataPoints);
+    console.log(dataPoints);
+    chart(dataPoints);
+  }
+});
 
-        
+
+// GET request when the page is load to initialize the value of the buttons
+$.ajax({
+  traditional: true,
+  url: '/initAtReset',
+  type: 'GET',
+  // contentType: 'application/json',
+  // dataType: 'json',
+  success: function(getData) {
+    dataFromServer = JSON.parse(getData);
+    console.log(getData);
+    evaluateState(getData);
+  }
+});
+
+//==========================================================================
+
+
 
 // The JSON sendData is posted to app.js
-function sendData(dataFromServer){
-	
-	  $.ajax({                       
-          traditional: true,
-          url: '/app',
-          type: 'POST',
-          contentType: 'application/json',
-          data: JSON.stringify(dataFromServer),
-          dataType: 'json',
-          success: function(response){ 
-		  console.log(response);
-		  evaluateState(response);		  
-		  }
-		  });
-		  }
+//==========================================================================
+function sendData(dataFromServer) {
 
- 
-// The function is executed when a buttorn/ range is clicked/changed. 	           
- function index(){                       
-   console.log("Hello");
-// Click source is identify based on the button ID e.g btn1, btn2..etc 
-  switch(event.srcElement.id)                                
-  {
- 
-  case "gpioPin12": dataFromServer.gpioPin12^= true;
-  break;
-  case "gpioPin11": dataFromServer.gpioPin11^= true;
-  break;
-  case "gpioPin10": dataFromServer.gpioPin10^= true;
-  break;
-  case "gpioPin09": dataFromServer.gpioPin09^= true;
-  break; 
-  case "BLDCslider": dataFromServer.motorSpeed= $(".slider").val();
-  break;
-  case "stopMotor": dataFromServer.motorSpeed= 1000;
-  break;
-  default: dataFromServer.ERR= true;
-  } 
+  $.ajax({
+    traditional: true,
+    url: '/app',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(dataFromServer),
+    dataType: 'json',
+    success: function(response) {
+      console.log(response);
+      evaluateState(response);
+    }
+  });
+}
+//==========================================================================
+
+
+// The function is executed when a buttorn/ range is clicked/changed.
+//==========================================================================
+function index() {
+  console.log("Hello");
+  // Click source is identify based on the button ID e.g btn1, btn2..etc
+  switch (event.srcElement.id) {
+
+    case "gpioPin12":
+      dataFromServer.gpioPin12 ^= true;
+      break;
+    case "gpioPin11":
+      dataFromServer.gpioPin11 ^= true;
+      break;
+    case "gpioPin10":
+      dataFromServer.gpioPin10 ^= true;
+      break;
+    case "gpioPin09":
+      dataFromServer.gpioPin09 ^= true;
+      break;
+    case "BLDCslider":
+      dataFromServer.motorSpeed = $(".slider").val();
+      break;
+    case "stopMotor":
+      dataFromServer.motorSpeed = 1000;
+      break;
+    default:
+      dataFromServer.ERR = true;
+  }
   sendData(dataFromServer);
 }
+//==========================================================================
 
 
 // This function update the HTML, CSS based on the actual state of the button, slider.
-function evaluateState(state){
-	state= JSON.parse(state);
-	
-if(state.gpioPin12==1)
-	{
-		$("#gpioPin12").html("Led is On");
-		$("#gpioPin12").css("background-color", "#5aa897");
-		$("#gpioPin12").css("color", "white");
-		
-    }
-    else{
-		$("#gpioPin12").html("Led is Off");
-		$("#gpioPin12").css("background-color", "WhiteSmoke");
-		$("#gpioPin12").css("color", "black");  
-	}
-	
-		
-		
-if(state.gpioPin11==1)
-	{
-		$("#gpioPin11").html("Inverter is On");
-		$("#gpioPin11").css("background-color", "#5aa897");
-		$("#gpioPin11").css("color", "white");
-		
-    }
-    else{
-		$("#gpioPin11").html("Inverter is Off");
-		$("#gpioPin11").css("background-color", "WhiteSmoke");
-		$("#gpioPin11").css("color", "black");  
-	}
-	
-	
-if(state.gpioPin10==1)
-	{
-		$("#gpioPin10").html("Port 10 is On");
-		$("#gpioPin10").css("background-color", "#5aa897");
-		$("#gpioPin10").css("color", "white");
-		
+//==========================================================================
+function evaluateState(state) {
+  state = JSON.parse(state);
 
-    }
-    else{
-		$("#gpioPin10").html("Port 10 is Off");
-		$("#gpioPin10").css("background-color", "WhiteSmoke");
-		$("#gpioPin10").css("color", "black");  
-	}
-	
-if(state.gpioPin09==1)
-	{
-		$("#gpioPin09").html("Port 09 is On");
-		$("#gpioPin09").css("background-color", "#5aa897");
-		$("#gpioPin09").css("color", "white");
-		
+  if (state.gpioPin12 == 1) {
+    $("#gpioPin12").html("Led is On");
+    $("#gpioPin12").css("background-color", "#5aa897");
+    $("#gpioPin12").css("color", "white");
 
-    }
-    else{
-		$("#gpioPin09").html("Port 09 is Off");
-		$("#gpioPin09").css("background-color", "WhiteSmoke");
-		$("#gpioPin09").css("color", "black");  
-	}
-	
-	$(".slider").val(state.motorSpeed);
-	};
-	
-	
+  } else {
+    $("#gpioPin12").html("Led is Off");
+    $("#gpioPin12").css("background-color", "WhiteSmoke");
+    $("#gpioPin12").css("color", "black");
+  }
+
+
+
+  if (state.gpioPin11 == 1) {
+    $("#gpioPin11").html("Inverter is On");
+    $("#gpioPin11").css("background-color", "#5aa897");
+    $("#gpioPin11").css("color", "white");
+
+  } else {
+    $("#gpioPin11").html("Inverter is Off");
+    $("#gpioPin11").css("background-color", "WhiteSmoke");
+    $("#gpioPin11").css("color", "black");
+  }
+
+
+  if (state.gpioPin10 == 1) {
+    $("#gpioPin10").html("Port 10 is On");
+    $("#gpioPin10").css("background-color", "#5aa897");
+    $("#gpioPin10").css("color", "white");
+
+
+  } else {
+    $("#gpioPin10").html("Port 10 is Off");
+    $("#gpioPin10").css("background-color", "WhiteSmoke");
+    $("#gpioPin10").css("color", "black");
+  }
+
+  if (state.gpioPin09 == 1) {
+    $("#gpioPin09").html("Port 09 is On");
+    $("#gpioPin09").css("background-color", "#5aa897");
+    $("#gpioPin09").css("color", "white");
+
+
+  } else {
+    $("#gpioPin09").html("Port 09 is Off");
+    $("#gpioPin09").css("background-color", "WhiteSmoke");
+    $("#gpioPin09").css("color", "black");
+  }
+
+  $(".slider").val(state.motorSpeed);
+};
+//==========================================================================
